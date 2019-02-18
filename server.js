4690267e94c3acf85bccf4dd6c8773ce2ea5617e
Daniel K <34487179+dmdddd@users.noreply.github.com>
2019-02-18 13:50:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ admin.initializeApp({
   credential: admin.credential.cert({
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
   }),
   databaseURL: "https://garageinc-fe238.firebaseio.com"
 });
